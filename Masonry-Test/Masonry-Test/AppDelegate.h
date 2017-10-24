95ebb55b3632e0174f8facd6c5cbcb635e332933
bosszhu <m18270822163@163.com>
2017-10-24 14:59:16 +0800

    1. 创建项目

---
+++ Masonry-Test/Masonry-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Masonry-Test
+//
+//  Created by yuntong on 2017/10/24.
+//  Copyright © 2017年 yuntong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
