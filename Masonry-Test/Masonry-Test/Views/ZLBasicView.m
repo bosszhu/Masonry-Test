488d15dd3058ee426c840a77140e87d94d853323
bosszhu <m18270822163@163.com>
2017-10-26 17:49:47 +0800

    1. 第二个View的布局

--- Masonry-Test/Masonry-Test/Views/ZLBasicView.m
+++ Masonry-Test/Masonry-Test/Views/ZLBasicView.m
@@ -19,10 +19,53 @@
         greenView.backgroundColor = [UIColor greenColor];
         greenView.layer.borderColor = [UIColor blackColor].CGColor;
         greenView.layer.borderWidth = 2;
+        [self addSubview:greenView];
         
+        UIView *redView = [UIView new];
+        redView.backgroundColor = [UIColor redColor];
+        redView.layer.borderColor = [UIColor blackColor].CGColor;
+        redView.layer.borderWidth = 2;
+        [self addSubview:redView];
         
-//        UIView *redView
-//        UIView *blueView
+        UIView *blueView = [UIView new];
+        blueView.backgroundColor = [UIColor blueColor];
+        blueView.layer.borderColor = [UIColor blackColor].CGColor;
+        blueView.layer.borderWidth = 2;
+        [self addSubview:blueView];
+        
+        UIView *superView = self;
+        int padding = 10;
+        
+        //MARK:布局子控件
+        [greenView makeConstraints:^(MASConstraintMaker *make) {
+            make.top.greaterThanOrEqualTo(superView.top).offset(padding);
+            make.left.equalTo(superView.left).offset(padding);
+            make.right.equalTo(redView.left).offset(-padding);
+            make.bottom.equalTo(blueView.top).offset(-padding);
+            
+            make.width.equalTo(redView.width);
+            make.height.equalTo(redView.height);
+            make.height.equalTo(blueView.height);
+        }];
+        
+        [redView makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(superView.mas_top).with.offset(padding);
+            make.left.equalTo(greenView.mas_right).offset(padding);
+            make.right.equalTo(superView.mas_right).offset(-padding);
+            make.bottom.equalTo(blueView.mas_top).offset(-padding);
+            
+            make.width.equalTo(greenView.width);
+            make.height.equalTo(@[greenView.height,blueView.height]);
+        }];
+        
+        [blueView makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(greenView.mas_bottom).offset(padding);
+            make.left.equalTo(superView.mas_left).offset(padding);
+            make.right.equalTo(superView.mas_right).offset(-padding);
+            make.bottom.equalTo(superView.mas_bottom).offset(-padding);
+            
+            make.height.equalTo(@[greenView.height,redView.height]);
+        }];
     }
     return self;
 }
