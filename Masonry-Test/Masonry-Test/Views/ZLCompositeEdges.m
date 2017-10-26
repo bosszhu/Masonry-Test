0cc275f987fbbb0102071bb6c5bfbd92b98b33af
bosszhu <m18270822163@163.com>
2017-10-26 18:42:11 +0800

    1. 边界布局

--- Masonry-Test/Masonry-Test/Views/ZLCompositeEdges.m
+++ Masonry-Test/Masonry-Test/Views/ZLCompositeEdges.m
@@ -10,12 +10,30 @@
 
 @implementation ZLCompositeEdges
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        UIView *lastView = self;
+        for (int i = 0; i < 10; ++i) {
+            UIView *view = [UIView new];
+            view.backgroundColor = [self setRandowColor];
+            view.layer.borderWidth = 2;
+            view.layer.borderColor = [UIColor blackColor].CGColor;
+            [self addSubview:view];
+            //布局
+            [view makeConstraints:^(MASConstraintMaker *make) {
+                make.edges.equalTo(lastView).insets(UIEdgeInsetsMake(5, 10, 15, 20));
+            }];
+            lastView = view;
+        }
+        
+        
+        
+    }
+    return self;
 }
-*/
-
+- (UIColor *)setRandowColor {
+    return [UIColor colorWithRed:((float)arc4random_uniform(256) / 255.0) green:((float)arc4random_uniform(256) / 255.0) blue:((float)arc4random_uniform(256) / 255.0) alpha:1.0];
+}
 @end
